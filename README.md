e5a39e87cdfeea2ed2cf3a0032abe71dac58be20
unknown <Shivraj_Rath@BLRKEC243890D.ad.infosys.com>
2015-06-09 18:35:02 +0530

    Removes the delay logic. It impacts multiple xhr requests made simultaneously

--- README.md
+++ README.md
@@ -12,12 +12,9 @@ This application gives your project easy setup for mock services. It supports la
 * If you want to run on different port run ``PORT=<new_port> npm start``
 
 ### How to use
-* GET Request
+* GET/POST Request
 Put your json file inside service folder in same structure as of your request URL
 e.g: ``get/project/all`` would be get (folder) / project (folder) / all.json
 You don't need to restart the app
 * Other Requests
-For requests like PUT/POST/DELETE etc you'll allways get a success response
-* Delayed Response
-Pass a 'delay' parameter to get the request after the supplied ms
-e.g ``get/project/all?delay=10000`` will respond after 10ms
+For requests like PUT/DELETE etc you'll allways get a success response
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*jshint node: true*/
 /**
  * Index.js
  * Creates an express app to manage mock response
@@ -9,62 +10,67 @@ var app = express();
 var constants = require('./constants.json');
 
 var call_param = '/',
-    port = constants.port,
-    delay = 0;
+  port = constants.port;
 
 /**
-* Allows access to all origin
-*/
-app.all("*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
- 
-    next();
+ * Allows access to all origin
+ */
+app.all("*", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  // Uncomment to allow with credentials. You'd need to give origin in that case
+  //res.header('Access-Control-Allow-Credentials', true);
+  next();
 });
- 
+
 /**
  * Returns a JSON service response by fetching the files in ./service folder
  */
-app.get('*', function(req, res) {
-    try {
-        call_param = req.params[0];
-        // Delay the response by the the time value
-        delay = req.query.delay;
+app.get('*', function (req, res) {
+  try {
+    call_param = req.params[0];
+    // Send file content as response
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile(__dirname + '/service' + call_param + '.json', {}, function (err) {
+      res.status(404).end(JSON.stringify(constants.ERR.File_Not_Found));
+    });
+  } catch (ex) {
+    res.status(404).end(ex.message);
+  }
+});
 
-        var timeoutObj = setTimeout(function() {
+app.post('*', function (req, res) {
+  try {
+    call_param = req.params[0];
+    // Send file content as response
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile(__dirname + '/service' + call_param + '.json', {}, function (err) {
+      res.status(404).end(JSON.stringify(constants.ERR.File_Not_Found));
+    });
 
-            // Send file content as response
-            res.setHeader('Content-Type', 'application/json');
-            res.sendFile(__dirname + '/service' + call_param + '.json', {}, function(err) {
-                res.status(404).end(JSON.stringify(constants.ERR.File_Not_Found));
-            });
-        }, delay);
-    } catch (ex) {
-        res.status(404).end(ex.message);
-    }
+  } catch (ex) {
+    res.status(404).end(ex.message);
+  }
 });
 
 /**
  * Sucess response for all non GET requests
  */
-app.all('*', function(req, res) {
-    try {
-        // Delay the response by the the time value
-        delay = req.query.delay;
-        var timeoutObj = setTimeout(function() {
-            res.status(200).end(JSON.stringify(constants.SUCCESS));
-        }, delay);
-    } catch (ex) {
-        res.status(404).end(ex.message);
-    }
-})
+app.all('*', function (req, res) {
+  try {
+    res.status(200).end(JSON.stringify(constants.SUCCESS));
+
+  } catch (ex) {
+    res.status(404).end(ex.message);
+  }
+});
 
 // Run the application on the port 
 if (app.get('env')) {
-    port = process.env.PORT || port;
+  port = process.env.PORT || port;
 }
 
-app.listen(port, function() {
-    console.log('App Started at port number ' + port);
+app.listen(port, function () {
+  console.log('App Started at port number ' + port);
 });
